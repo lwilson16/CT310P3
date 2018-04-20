6cc114608b07b2793d7d31bde71597d8cf819eef
lwilson16 <35437792+lwilson16@users.noreply.github.com>
2018-04-19 18:45:31 -0600

    Add files via upload

--- allstatus.php
+++ allstatus.php
@@ -3,10 +3,23 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 <body>
+	<div>
+		<p id="response">
+		Print response here
+		</p>
+	</div>
 <script>
 	$.get("/~ct310/yr2018sp/master.json", function(data){
 		//var response = JSON.parse(data);
-		console.log(data[0]);
+		//console.log(data);
+		for(i = 0; i < data.length; i++){
+			var eid = data[i].eid;
+			console.log(eid);
+			$.get("/~" + eid + "/ct310/index.php/federation/status", function(response){	
+				console.log(response);
+				$('#response').append(response.status);
+			});
+		}	
 	});
 </script>
 </body>
