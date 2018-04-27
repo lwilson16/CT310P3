099ab3848952d8cea649435f9ce3a74b4a954272
YasminAlshafai <36975887+YasminAlshafai@users.noreply.github.com>
2018-04-26 19:56:50 -0600

    Add files via upload
    this is the federation file with the attractions listing action

--- federation.php
+++ federation.php
@@ -1,6 +1,7 @@
-<?php
-class Controller_Federation extends Controller{
 
+<?php
+use Model\florida;
+class Controller_Federation extends Controller{
  public function action_status(){
 	
 	$object = new stdClass();
@@ -14,5 +15,34 @@ class Controller_Federation extends Controller{
 	  $content = View::forge('federations/allstatus');  
 		return $content;
   }
+  
+  public function action_listing(){
+  $listingName = new florida();
+  $attractions = Florida::getAttraction();
+  
+  //loop through attractions
+  $listings = "";
+  $listingsArr = [];
+  for($x=0;$x<sizeof($attractions);$x++){
+	 $obj= new stdClass();
+	
+	 $obj->id = $attractions[$x]['attractionID'];
+	 $obj->name = $attractions[$x]['attractionName'];
+	$obj->state='Florida';
+	 
+	 //$object_JSON = Format::forge($obj)->to_json();
+	 
+	 //$listings. = $object_JSON;
+	 array_push ($listingsArr, $obj); 
+	  
+  }
+  
+  $object_JSON = Format::forge($listingsArr)->to_json();
+  //append attractins as a string 
+  $response = Response::forge($object_JSON);
+	$response->set_header('Content-Type', 'application/json');
+	return $response;
+  }
+
 }
 ?>
