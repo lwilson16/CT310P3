33925ccf3950ed84071e681ba466ab5da6bd26ad
lwilson16 <35437792+lwilson16@users.noreply.github.com>
2018-04-26 18:30:01 -0600

    Add files via upload

--- federation.php
+++ federation.php
@@ -1,10 +1,14 @@
 <?php
 class Controller_Federation extends Controller{
-  public function action_status(){
+
+ public function action_status(){
+	
 	$object = new stdClass();
 	$object->status = 'closed';
 	$object_JSON = Format::forge($object)->to_json();
-	return $object_JSON;
+	$response = Response::forge($object_JSON);
+	$response->set_header('Content-Type', 'application/json');
+	return $response;
   }
   public function action_allstatus(){
 	  $content = View::forge('federations/allstatus');  
