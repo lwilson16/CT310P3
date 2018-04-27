bc29c8738fd95800e7559f555ea8fda1e235a2a4
lwilson16 <35437792+lwilson16@users.noreply.github.com>
2018-04-26 20:47:29 -0600

    Add files via upload

--- federation.php
+++ federation.php
@@ -17,6 +17,34 @@ class Controller_Federation extends Controller{
 		return $content;
   }
 
+ public function action_listing(){
+  $listingName = new florida();
+  $attractions = Florida::getAttraction();
+  
+  //loop through attractions
+  $listings = "";
+  $listingsArr = [];
+  for($x=0;$x<sizeof($attractions);$x++){
+	 $obj= new stdClass();
+	
+	 $obj->id = $attractions[$x]['attractionID'];
+	 $obj->name = $attractions[$x]['attractionName'];
+	$obj->state='Florida';
+	 
+	 //$object_JSON = Format::forge($obj)->to_json();
+	 
+	 //$listings. = $object_JSON;
+	 array_push ($listingsArr, $obj); 
+	    }
+  
+  	$object_JSON = Format::forge($listingsArr)->to_json();
+  	//append attractins as a string 
+ 	 $response = Response::forge($object_JSON);
+	$response->set_header('Content-Type', 'application/json');
+	return $response;
+  }
+
+
   public function action_attraction($attractionID){
 	$object = new stdClass();
         $Florida = new florida();
