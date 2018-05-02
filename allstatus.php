700c1d352b7a7ed2920c3e9ebcaa53c43b9c9e86
lwilson16 <35437792+lwilson16@users.noreply.github.com>
2018-05-01 19:47:22 -0600

    Add files via upload

--- allstatus.php
+++ allstatus.php
@@ -1,48 +1,92 @@
 <html>
-	<style>
+
+<style>
+body{
+	background-color: grey !important;
+}
 	
-	#response1{
+#open{
 	color: green;
-	}
-	</style>
+}
+
+#closed{
+	color: red;
+	
+}
+	
+#noResponse{
+	color: yellow;
+}
+	
+</style>
+
 <head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
 </head>
 <body>
-	<div>
+	<div class="container">
+		<div class="jumbotron">
+			<h1>  All Status </h1>
+		</div>
 		<p id="response">
-		Print response here
 		</p>
 	</div>
 <script>
-	$.get("/~ct310/yr2018sp/master.json", function(data){
-		//var response = JSON.parse(data);
-		//console.log(data);
-		for(i = 0; i < data.length; i++){
-			var eid = data[i].eid;
-			console.log(eid);
-			$.get("/~" + eid + "/ct310/index.php/federation/status", function(response){	
-				console.log(response);
+$.get("/~ct310/yr2018sp/master.json", function(data){
+	$(data).each(function(index, value){
+		console.log(value);
+		var eid = value.eid;
+		if( eid == ""){
+			$('<p>').attr('id', 'noResponse').text("404 Server error: " + " " + "team: " + value.team ).appendTo('#response');
+
+		}
+	$.get("/~" + eid + "/ct310/index.php/federation/status",  function(response, textStatus, xhr){	
+				console.log(xhr.status);
+				//console.log(eid);	
 				//Checking the variable type
-				if (jQuery.type(response)==="string"){
-				response = JSON.parse(response);
-				//$('#response').append(response.status);
-				$('<p>').attr('id', 'response1').text(response.status).appendTo('#response');
-			}else {
-				//$('#response').append(response.status);
-				$('<p>').attr('id', 'response1').text(response.status).appendTo('#response');
+				if(jQuery.type(response) === "object"){
 				
-			}
-				//changing the color of the responses 
-				//open is green
-				//closed in yellow
-				//need to put the response.status inside a paragraph tag and change its color
-				//$('#response').append('<p id="response1"></p>');
-				//$('<p>').attr('id', 'response1').appendTo('#response');
+					if(response.status == "open"){
+
+						$('<p>').attr('id', 'open').text("nameShort: " + value.nameShort + " " + "nameLong: " + value.nameLong + " "+  response.status).appendTo('#response');
+					}
+
+					else if(response.status == "closed"){
+
+						$('<p>').attr('id', 'closed').text("nameShort: " + value.nameShort + " " + "nameLong: " + value.nameLong + " "+  response.status).appendTo('#response');
+
+					}
+
+
 				
-			});
-		}	
-	});
+				}
+				else{
+					if (jQuery.type(response)==="string"){
+						try{
+							response = JSON.parse(response);
+						}
+						catch(err){
+							$('<p>').attr('id', 'noResponse').text("nameShort: " + value.nameShort + " " + "nameLong: " + value.nameLong + " " + "ERROR: Not JSON object OR JSON String" ).appendTo('#response');
+						}
+						if(response.status == "open"){
+							$('<p>').attr('id', 'open').text("nameShort: " + value.nameShort + " " + "nameLong: " + value.nameLong + " "+  response.status).appendTo('#response');
+						}
+						else if(response.status == "closed"){
+
+							$('<p>').attr('id', 'closed').text("nameShort: " + value.nameShort + " " + "nameLong: " + value.nameLong + " "+  response.status).appendTo('#response');
+						}
+				
+					}
+
+			}				
+		});
+	})	
+})
 </script>
 </body>
 </html>
