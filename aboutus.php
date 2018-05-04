a116b23b54ea9f54f2a5260bb231ba4298842aa3
YasminAlshafai <36975887+YasminAlshafai@users.noreply.github.com>
2018-05-04 14:41:08 -0600

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>About us</title>
+
+</head>
+<body>
+<!-- Indicators -->
+	<div class="container text-center">
+        <h3> Yasmin Alshafai </h3>
+			<p>
+			I am going to graduate next week and I am not sure what is going to happen next.
+           </p>
+	</div>
+
+	<div class="container text-center">
+	<h3> Lettia Wilson </h3>
+		<p> My name is Lettia Wilson. Born and and raised in Denver, Colorado. I'm majoring in Applied Computing Technology with a concentration in Computing Technology. I hope to get a Masters of Science in Cybersecurity at the University of Denver and further my career in cyber security</p>
+	</div>
+</body>
+</html>
