d0a667495a3bf45b77835cc734cad3dbd3cbd952
lwilson16 <35437792+lwilson16@users.noreply.github.com>
2018-05-04 14:52:18 -0600

    Add files via upload

--- listingView.php
+++ listingView.php
@@ -7,28 +7,31 @@
 </head>
 
 <body>
-	<div class="desc">
+	
+	<div class='attrPic container text-center'>
+
+	<img src= "<?='/~' .$eid. "/ct310/index.php/federation/attrimage" . '/' .$id?>"/>
+	 			
 	</div>
-	<div class="name">
+	
+	<div class="desc containter text-center">
+
 	</div>
+
 <script>
-
 var id = '<?php echo $id ?>';
-
 var eid = '<?php echo $eid ?>';
-
 //make an ajax call to attractions api, for eid and passing id inside 
 $.get( "/~" + eid + "/ct310/index.php/federation/attraction" + '/' + id, function ( data ) {
 	//alert(data);
+	document.title = data.name;
 	$( ".desc" )
-	  .append( "Name: " + data.name ) 
-	  .append( "Description: " + data.desc ) 
+	  .append( "<h3>" + data.name + "</h3>" + "<br />") 
+	  .append(data.desc) 
 }, "json" );
 </script>	  
 
 <div>
-
-<img src= "<?='/~' .$eid. "/ct310/index.php/federation/attrimage" . '/' .$id?>"/>
 
 </div>
 
@@ -42,3 +45,4 @@ $.get( "/~" + eid + "/ct310/index.php/federation/attraction" + '/' + id, functio
 
 
 </html>
+
